bb9e79a13167b2b4222706d0f7b7065116f330f1
hcbrady <46352632+hcbrady@users.noreply.github.com>
2019-08-11 14:32:26 +1200

    Add files via upload

---
+++ skifield.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE instructors;
+DROP TABLE lift_operators;
+DROP TABLE skiers;
+DROP TABLE lessons;
+DROP TABLE chair_lifts;
+DROP TABLE weather;
+
+CREATE TABLE instructors
+       (ID   VARCHAR2(15) NOT NULL UNIQUE,
+       
